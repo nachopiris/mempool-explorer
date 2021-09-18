6603c0c9de0801da6a0dfff637ac3fb7797bf2fb
Ignacio <ignaciopirisb@gmail.com>
2021-09-18 18:21:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const Web3 = require('web3')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const { PROVIDER_URI, ROUTER } = process.env
+
+const options = {
+  timeout: 30000,
+  clientConfig: {
+    maxReceivedFrameSize: 100000000,
+    maxReceivedMessageSize: 100000000,
+  },
+  reconnect: {
+    auto: true,
+    delay: 5000,
+    maxAttempts: 15,
+    onTimeout: false,
+  },
+}
+
+const web3 = new Web3(
+  new Web3.providers.WebsocketProvider(PROVIDER_URI, options),
+)
+const subscription = web3.eth.subscribe('pendingTransactions', (err, res) => {
+  if (err) console.error(err)
+})
+
+const init = () => {
+  subscription.on('data', (txHash) => {
+    setTimeout(async () => {
+      try {
+        const tx = await web3.eth.getTransaction(txHash)
+        if (tx && tx.to === ROUTER && tx.input.slice(0, 10) === '0xf305d719') {
+          console.log(tx, new Date())
+        }
+      } catch (err) {
+        console.error(err)
+      }
+    })
+  })
+}
+
+init()
